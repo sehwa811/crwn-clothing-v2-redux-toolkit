9f6228f86b6b73bbabc9b29cc5464b8c325a5383
sehwa811 <wsd08111@gmail.com>
2023-09-07 14:01:35 +0900

    add payments

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,16 +1,22 @@
-import { USER_ACTION_TYPES } from './user.types';
+import { createSlice } from "@reduxjs/toolkit";
+
+//import { USER_ACTION_TYPES } from './user.types';
 
 const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      return state;
+export const userSlice = createSlice({
+  name: 'users',
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload
+      //값을 직접 변경하는 것처럼 보이지만, 실제로는 아니다. 
+    }
   }
-};
+});
+
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
